e2cf1ba777328d888f449712c5a65633ab5e1010
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-04-14 11:14:23 +0200

    clean code correction

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
-function render(){
-    loadpkm();
-}
+function render() {
+    loadPokemon();
+  }
+  
+async function getPokemonData(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const pokemonData = await response.json();
+    return pokemonData;
+  }
 
-
-async function loadpkm(){
-    for (let i = 1; i < 10; i++) {
-        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        let response = await fetch (url);
-        let responseAsJson = await response.json();
-        console.log('API answers:', responseAsJson['forms']['0']['name']);
-        let pkmSprites= responseAsJson['sprites']['front_default'];
-        let pkmNames= responseAsJson['forms']['0']['name'];
-        document.getElementById('all-Pokemon').innerHTML += generatePokemonHTML(pkmSprites, pkmNames, i);
-    }
-    }
-
-
-function generatePokemonHTML(pkmSprites, pkmNames, i) {
-        return `
-          <div class="single-Pokemon flex-center flex-column" id="single-Pokemon${i}" onclick="infoPkm(${i})">
-            <img src="${pkmSprites}" alt="">
-            <div class="flex-around">
-              <span>${pkmNames}</span>
-              <span class="margin-Left10">#${i}</span>
-            </div>
-          </div>
-        `;
+    async function loadPokemon() {
+        const allPokemonElement = document.getElementById('all-Pokemon');
+        for (let i = 1; i <= 9; i++) {
+          const pokemonData = await getPokemonData(i);
+          const pokemonHTML = generatePokemonHTML(pokemonData, i);
+          allPokemonElement.innerHTML += pokemonHTML;
+        }
       }
+  
+  function generatePokemonHTML(pokemonData, id) {
+    const pkmSprites = pokemonData.sprites.front_default;
+    const pkmNames = pokemonData.forms[0].name;
+    return `
+      <div class="single-Pokemon flex-center flex-column" id="single-Pokemon${id}" onclick="infoPkm(${id})">
+        <img src="${pkmSprites}" alt="">
+        <div class="flex-around">
+          <span>${pkmNames}</span>
+          <span class="margin-Left10">#${id}</span>
+        </div>
+      </div>
+    `;
+  }
+  
 
-function infoPkm(singlePkM){
-    console.log (singlePkM)
-}
+  async function infoPkm(id) {
+    const pokemonData = await getPokemonData(id);
+    const pkmSprites = pokemonData.sprites.front_default;
+    document.getElementById('pkm-img-big').style.backgroundImage = `url(${pkmSprites})`;
 
+    const pkmNames = pokemonData.forms[0].name;
+    document.getElementById('pkm-Names').innerHTML = `#${id} ${pkmNames}`
+
+  }
+  
+
+
+  
