6d58804239f76abc093f4a47aa4b4a680af31cbc
0xHaoMoon <122043813+0xHaoMoon@users.noreply.github.com>
2023-04-15 14:38:03 +0200

    create load function information

--- script.js
+++ script.js
@@ -8,9 +8,17 @@ async function getPokemonData(id) {
     return pokemonData;
   }
 
+  async function getPokemonSpecies(id) {
+    const response = await fetch(`  https://pokeapi.co/api/v2/pokemon-species/${id}/`);
+    const pokemonSpecies = await response.json();
+    return pokemonSpecies;
+  }
+
+
+
     async function loadPokemon() {
         const allPokemonElement = document.getElementById('all-Pokemon');
-        for (let i = 1; i <= 9; i++) {
+        for (let i = 1; i <= 10; i++) {
           const pokemonData = await getPokemonData(i);
           const pokemonHTML = generatePokemonHTML(pokemonData, i);
           allPokemonElement.innerHTML += pokemonHTML;
@@ -33,14 +41,61 @@ async function getPokemonData(id) {
   
 
   async function infoPkm(id) {
+    loadSprite(id)
+    loadName(id)
+    loadSpecies(id);
+    loadTypes(id);
+    loadHT(id);
+    loadWT(id);
+  }
+
+
+  async function loadName(id) {
+    const pokemonData = await getPokemonData(id);
+    const pkmNames = pokemonData.forms[0].name;
+    document.getElementById('pkm-Names').innerHTML = `${pkmNames}`;
+    document.getElementById('pkm-id').innerHTML = `#${id}`;
+  }
+
+
+  async function loadSprite(id) {
     const pokemonData = await getPokemonData(id);
     const pkmSprites = pokemonData.sprites.front_default;
     document.getElementById('pkm-img-big').style.backgroundImage = `url(${pkmSprites})`;
+  }
 
-    const pkmNames = pokemonData.forms[0].name;
-    document.getElementById('pkm-Names').innerHTML = `#${id} ${pkmNames}`
+
+  async function loadSpecies(id){
+    const getPokemonDataSpecies = await getPokemonSpecies(id);
+    const pkmSpecies = getPokemonDataSpecies.flavor_text_entries[0].flavor_text;
+    document.getElementById('info-Text').innerHTML = `${pkmSpecies}`;
+  }
+
+
+  async function loadTypes(id){
+    const pokemonData = await getPokemonData(id);
+    const pkmType1 = pokemonData.types[0].type.name;
+    const pkmType2 = pokemonData.types[1].type.name;
+    
+    document.getElementById('type1').innerHTML = `${pkmType1}`;
+    document.getElementById('type2').innerHTML = `${pkmType2}`;
+  }
+
+  async function loadWT(id){
+    const pokemonData = await getPokemonData(id);
+    const WT = pokemonData.height;
+    document.getElementById('WT').innerHTML = `${WT}`;
 
   }
+
+  async function loadHT(id){
+    const pokemonData = await getPokemonData(id);
+    const HT = pokemonData.weight;
+    document.getElementById('HT').innerHTML = `${HT}`;
+
+  }
+
+
   
 
 
